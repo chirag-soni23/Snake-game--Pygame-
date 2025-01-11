481a7e71e910bd6fa3c7a9a2db45085ba276d28b
Chirag Soni <csoni0693@gmail.com>
2025-01-11 11:41:29 +0530

    snake game

--- SnakeGame.py
+++ SnakeGame.py
@@ -18,17 +18,36 @@ pygame.display.update()
 # game specific variables
 exit_game = False
 game_over = False
+snake_x = 45
+snake_y = 55
+snake_size = 10
+fps = 30
 
+# clock
+clock = pygame.time.Clock()
 
 # creating a game loop
 while not exit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit_game = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                snake_x += 10    
+            if event.key == pygame.K_LEFT:
+                snake_x -= 10    
+            if event.key == pygame.K_UP:
+                snake_y -= 10    
+            if event.key == pygame.K_DOWN:
+                snake_y += 10    
+            
     
     
-    gameWindow.fill(white) 
+    gameWindow.fill(white)
+    # snake head
+    pygame.draw.rect(gameWindow,black,[snake_x,snake_y,snake_size,snake_size]) 
     pygame.display.update()
+    clock.tick(fps)
 
 
 # quit game
