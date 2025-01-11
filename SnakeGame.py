aef926b6e9654b4d99edf076bfffb94ea8385d02
Chirag Soni <csoni0693@gmail.com>
2025-01-11 12:21:23 +0530

    snake game

--- SnakeGame.py
+++ SnakeGame.py
@@ -19,14 +19,18 @@ pygame.display.update()
 # game specific variables
 exit_game = False
 game_over = False
+score = 0
 snake_x = 45
+snake_y = 55
 velocity_x = 0
 velocity_y = 0
-snake_y = 55
+init_velocity = 5
+
+food_x = random.randint(20, int(screen_width / 2))
+food_y = random.randint(20, int(screen_height / 2))
+
 snake_size = 10
-fps = 30
-food_x = random.randint(0,screen_width)
-food_y = random.randint(0,screen_height)
+fps = 60
 
 # clock
 clock = pygame.time.Clock()
@@ -38,21 +42,27 @@ while not exit_game:
             exit_game = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                velocity_x = 10
+                velocity_x = init_velocity
                 velocity_y = 0   
             if event.key == pygame.K_LEFT:
-                velocity_x = -10  
+                velocity_x = -init_velocity 
                 velocity_y = 0  
             if event.key == pygame.K_UP:
-                velocity_y = -10 
+                velocity_y = -init_velocity
                 velocity_x = 0   
             if event.key == pygame.K_DOWN:
-                velocity_y = 10  
+                velocity_y = init_velocity  
                 velocity_x = 0 
             
     
     snake_x += velocity_x
     snake_y += velocity_y
+    
+    if abs(snake_x - food_x) < 6 and abs(snake_y - food_y) < 66:
+        score += 1
+        print(f"Score: {score * 10}")
+        food_x = random.randint(20, int(screen_width / 2))
+        food_y = random.randint(20, int(screen_height / 2))    
     gameWindow.fill(white)
     # snake food
     pygame.draw.rect(gameWindow,red,[food_x,food_y,snake_size,snake_size])
