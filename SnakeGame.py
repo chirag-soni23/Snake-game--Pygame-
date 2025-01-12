f19533a82dbefc23c1b72d595f7c6a56aa0ba2f0
Chirag Soni <csoni0693@gmail.com>
2025-01-12 11:06:01 +0530

    snake game

--- SnakeGame.py
+++ SnakeGame.py
@@ -4,7 +4,7 @@ import random
 pygame.init()
 
 # Colors
-white = (255, 255, 255)
+white = (233,220,229)
 red = (255, 0, 0)
 black = (0, 0, 0)
 green = (0, 255, 0)
@@ -12,7 +12,6 @@ green = (0, 255, 0)
 # Screen dimensions
 screen_width = 900
 screen_height = 600
-
 # Creating window
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Snake Game")
